d41a48b531cb680c956691bc9f859420eb168ea0
fuwenke <fuwen163.com@qq.com>
2022-10-16 21:50:16 +0800

    创建了一个Person类，有一个无参构造器

---
+++ TestModule01/src/com/fwk/Person.java
@@ -0,0 +1,13 @@
+package com.fwk;
+
+/**
+ * @author : fwk
+ * @date: 2022/10/16 - 10 - 16 - 21:49
+ * @Description: com.fwk
+ * @version: 1.0
+ */
+public class Person {
+    public Person(){
+
+    }
+}
